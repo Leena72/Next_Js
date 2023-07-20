2c798a6b219f4d04357040d260a1dfa492e4508d
leena72 <leena.sharma72@gmail.com>
2023-07-20 12:03:12 +0530

    Dashboard Ui update

--- src/container/Header.js
+++ src/container/Header.js
@@ -1,9 +1,15 @@
 'use client'
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import Image from 'next/image'
 import logo from "../Assets/images/bharti-axa-logo.png";
 
 const Header = (props) => {
+    const [islogin, setislogin] = useState(null)
+    useEffect(() => {
+        let test= localStorage.getItem('isLogin')
+        setislogin(test)
+    })
+    
     return (
         <div className='header-container'>
             <div className='header-logo'>
@@ -13,7 +19,7 @@ const Header = (props) => {
                 />
             </div>
             {
-                false &&
+              islogin &&
                 <div className='header-content'>
                     <p>Proposal Number</p>
                     <p>{123456}</p>
