a1b06262f0f1620df4b78f496ba7a6b48b3571e1
leena72 <leena.sharma72@gmail.com>
2023-10-05 17:04:34 +0530

    update fixes status

--- src/redux/reducer/customerDetailReducer.js
+++ src/redux/reducer/customerDetailReducer.js
@@ -1,6 +1,6 @@
 import { statusApi } from "@/data";
 const initialState = {};
-const customerDetailReducer = (state = statusApi, action) => {
+const customerDetailReducer = (state = initialState, action) => {
   switch (action.type) {
     case "CUSTOMER_INFO_SUCCESS":
       return (action.info ? action.info.body : state);
